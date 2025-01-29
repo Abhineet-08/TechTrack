47e68af5dcb64d76c207efef434ddd1a8579b22f
Abhineet-08 <113853385+Abhineet-08@users.noreply.github.com>
2025-01-29 19:21:58 +0530

    abhineet3rdcommit

--- app/src/main/java/com/devzone/techtrack/ui/SignUpActivity.kt
+++ app/src/main/java/com/devzone/techtrack/ui/SignUpActivity.kt
@@ -3,6 +3,7 @@ package com.devzone.techtrack.ui
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.devzone.techtrack.R
 
 class SignUpActivity : AppCompatActivity() {
@@ -10,6 +11,8 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
 
     }
 }
