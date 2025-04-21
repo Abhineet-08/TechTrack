2a72c70422c31753d5caf2f3c222918d7c349956
khushi kumari <khushikumari1723@gmail.com>
2025-04-21 22:16:05 +0530

    khushi2stcommit

--- app/src/main/java/com/devzone/techtrack/CoursesActivity.kt
+++ app/src/main/java/com/devzone/techtrack/CoursesActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.devzone.techtrack.databinding.ActivityCoursesBinding
-//import com.devzone.techtrack.ui.OSActivity
+import com.devzone.techtrack.ui.OSActivity
 
 class CoursesActivity : AppCompatActivity() {
 
@@ -15,9 +15,9 @@ class CoursesActivity : AppCompatActivity() {
         binding = ActivityCoursesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // OS course click
-//        binding.courseOS.setOnClickListener {
-//            startActivity(Intent(this, OSActivity::class.java))
-//        }
+        // OS course click listener
+        binding.courseOS.setOnClickListener {
+            startActivity(Intent(this, OSActivity::class.java))
+        }
     }
 }
