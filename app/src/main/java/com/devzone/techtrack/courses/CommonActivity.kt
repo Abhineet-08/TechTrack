5820ffba25c7ae7ed3f64d1a5eedfa86793889a9
Abhineet-08 <113853385+Abhineet-08@users.noreply.github.com>
2025-04-28 12:49:53 +0530

    abhineet13thcommit

--- app/src/main/java/com/devzone/techtrack/courses/CommonActivity.kt
+++ app/src/main/java/com/devzone/techtrack/courses/CommonActivity.kt
@@ -1,8 +1,12 @@
 package com.devzone.techtrack.courses
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.devzone.techtrack.R
+import com.devzone.techtrack.courses.common.LecturesActivity
+import com.devzone.techtrack.courses.common.ResourcesActivity
+import com.devzone.techtrack.courses.common.RoadMapActivity
 import com.devzone.techtrack.databinding.ActivityCommonBinding
 
 class CommonActivity : AppCompatActivity() {
@@ -20,21 +24,25 @@ class CommonActivity : AppCompatActivity() {
                 binding.courseTagline.setText(R.string.tag_os)
                 binding.courseFooter.setText(R.string.foot_os)
             }
+
             "DBMS" -> {
                 binding.courseTitle.setText(R.string.course_dbms)
                 binding.courseTagline.setText(R.string.tag_dbms)
                 binding.courseFooter.setText(R.string.foot_dbms)
             }
+
             "Web" -> {
                 binding.courseTitle.setText(R.string.course_web)
                 binding.courseTagline.setText(R.string.tag_web)
                 binding.courseFooter.setText(R.string.foot_web)
             }
+
             "Android" -> {
                 binding.courseTitle.setText(R.string.course_android)
                 binding.courseTagline.setText(R.string.tag_android)
                 binding.courseFooter.setText(R.string.foot_android)
             }
+
             "DSA" -> {
                 binding.courseTitle.setText(R.string.course_dsa)
                 binding.courseTagline.setText(R.string.tag_dsa)
@@ -42,6 +50,23 @@ class CommonActivity : AppCompatActivity() {
             }
         }
 
+        binding.btnWatchNow.setOnClickListener {
+            val intent = Intent(this, LecturesActivity::class.java)
+            intent.putExtra("courseName", courseName) // Passing course name
+            startActivity(intent)
+        }
+
+        binding.btnViewRoadmap.setOnClickListener {
+            val intent = Intent(this, RoadMapActivity::class.java)
+            intent.putExtra("courseName", courseName) // Passing course name
+            startActivity(intent)
+        }
+
+        binding.btnExploreResources.setOnClickListener {
+            val intent = Intent(this, ResourcesActivity::class.java)
+            intent.putExtra("courseName", courseName) // Passing course name
+            startActivity(intent)
+        }
 
 
         binding.backBtn.setOnClickListener {
